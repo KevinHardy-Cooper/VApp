5f2050b49d3c41c199819528b973d067869e5b0e
KevinHardy-Cooper <11304398+KevinHardy-Cooper@users.noreply.github.com>
2019-01-10 13:01:51 -0500

    Included SQL file that contains commands that can set up required tables and values, as well as updated the README (#8)

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # SFWRENG_4G06
 This will be the private repo for our SFWR ENG 4G06 capstone project. The group members for the project will be able to commit to the repo.
+
+# How to Set Up The Database
+## Remote Database
+TBD
+## Local Database
+### Installation
+1. Download [MySQL](https://www.mysql.com) for your respective machine. 
+2. Follow their setup instructions in order to install it on your machine. 
+### Getting Started
+1. Fire up the MySQL Server (there's usually a "Start MySQL Server" button somewhere). 
+2. Once the server is started, open up a command prompt and type `mysql -u root -p` which will then require your 
+password. You may have to set up credentials so that you can use this command on your machine.
+    * If you are using Git Bash for Windows, you may have to type `winpty mysql -u root -p`
+3. The `mysql` prompt should now show up. Now type `create database VApp;`. This command will create the database that 
+we will load the `database.sql` file into. I couldn't think of anything else to call it :).
+4. Now enter `exit`; We are now going to load the database from the `database.sql` file, so hopefully you remember the
+path to that file. 
+5. `mysql -u root -p VApp < full/path/to/database.sql`. Enter your password once more, and the console 
+should freeze for a bit as it executes the SQL. 
+    * On Windows, to import the file:
+    * Copy the `database.sql` file to `C:\Program Files\MySQL\MySQL Shell 8.0\bin\`
+    * In Git Bash, type `winpty mysql -u root -p`
+    * Enter your password
+    * Type `use VApp;`
+    * Type `source database.sql`
+6. Now log in again and check if it worked: `mysql -u root -p` or `winpty mysql -u root -p` for Windows. Use  
+`show databases;` and verify that VApp is there. 
+7. `use VApp;`. We are selecting this database to use from hereon.
+8. `show tables;`. You should see 7 tables (Implications, Score_Types, Scores,
+Setting_States, Settings, Social Media, Users). 
+9. Let's run a query on one to see if it filled correctly.
+`select * from Score_Types;`. The query should return two records, `cumulative` and `twitter`. And that's it! 
+10. Exit the prompt using `exit;`, and turn off the MySQL server if you are done using it (Look for the button that says "Stop MySQL 
+Server", should be in the window where you started the server from).
+
+NOTE:
+If you have made change to a table, such as inserted Implications or Settings, export the database as an SQL file and 
+make a pull request on GitHub with the updated file.
---
+++ database.sql
@@ -0,0 +1,233 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `VApp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Implications`
+--
+
+CREATE TABLE `Implications` (
+  `id` int(11) NOT NULL,
+  `setting_id` int(11) NOT NULL,
+  `description` mediumtext NOT NULL,
+  `instructions` mediumtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Scores`
+--
+
+CREATE TABLE `Scores` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `type_id` int(11) NOT NULL,
+  `score` int(5) NOT NULL,
+  `time` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Score_Types`
+--
+
+CREATE TABLE `Score_Types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Score_Types`
+--
+
+INSERT INTO `Score_Types` (`id`, `name`) VALUES
+(1, 'cumulative'),
+(2, 'twitter');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Settings`
+--
+
+CREATE TABLE `Settings` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `social_media_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Setting_States`
+--
+
+CREATE TABLE `Setting_States` (
+  `id` int(11) NOT NULL,
+  `setting_id` int(11) NOT NULL,
+  `state` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Social_Media`
+--
+
+CREATE TABLE `Social_Media` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Social_Media`
+--
+
+INSERT INTO `Social_Media` (`id`, `name`) VALUES
+(1, 'twitter');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `session_id` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Implications`
+--
+ALTER TABLE `Implications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `setting_id` (`setting_id`);
+
+--
+-- Indexes for table `Scores`
+--
+ALTER TABLE `Scores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `type_id` (`type_id`);
+
+--
+-- Indexes for table `Score_Types`
+--
+ALTER TABLE `Score_Types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Settings`
+--
+ALTER TABLE `Settings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `social_media_id` (`social_media_id`);
+
+--
+-- Indexes for table `Setting_States`
+--
+ALTER TABLE `Setting_States`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `setting_id` (`setting_id`);
+
+--
+-- Indexes for table `Social_Media`
+--
+ALTER TABLE `Social_Media`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Implications`
+--
+ALTER TABLE `Implications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Scores`
+--
+ALTER TABLE `Scores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Score_Types`
+--
+ALTER TABLE `Score_Types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `Settings`
+--
+ALTER TABLE `Settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Setting_States`
+--
+ALTER TABLE `Setting_States`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Social_Media`
+--
+ALTER TABLE `Social_Media`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Implications`
+--
+ALTER TABLE `Implications`
+  ADD CONSTRAINT `implications_ibfk_1` FOREIGN KEY (`setting_id`) REFERENCES `Settings` (`id`);
+
+--
+-- Constraints for table `Scores`
+--
+ALTER TABLE `Scores`
+  ADD CONSTRAINT `scores_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`),
+  ADD CONSTRAINT `scores_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `Score_Types` (`id`);
+
+--
+-- Constraints for table `Settings`
+--
+ALTER TABLE `Settings`
+  ADD CONSTRAINT `settings_ibfk_1` FOREIGN KEY (`social_media_id`) REFERENCES `Social_Media` (`id`);
+
+--
+-- Constraints for table `Setting_States`
+--
+ALTER TABLE `Setting_States`
+  ADD CONSTRAINT `setting_states_ibfk_1` FOREIGN KEY (`setting_id`) REFERENCES `Settings` (`id`);
