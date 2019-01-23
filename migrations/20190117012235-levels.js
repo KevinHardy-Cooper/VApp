6f1e8c047dcf9d373db75bfd370e1b640d3ee959
Terin Dhadda <terindhadda@hotmail.com>
2019-01-22 19:26:40 -0500

    fix type (#29)
    alpha

--- migrations/20190117012235-levels.js
+++ migrations/20190117012235-levels.js
@@ -16,7 +16,7 @@ exports.setup = function(options, seedLink) {
 
 exports.up = function(db) {
   return db.createTable('Levels', {
-    id: { type: 'int', primaryKey: true },
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
     name: 'string',
     amount: 'int'
   });
